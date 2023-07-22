e82c16fc202b5b050f359d2d34dfaf0be5f47b2c
Anam Ali <anamali4620@gmail.com>
2023-07-23 00:05:03 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,4 @@
+export default function About() {
+  return <h3>We deliver Enduring Results
+  We combine Celebal Tech's global expertise with deep local insights to help our clients achieve their biggest goals. As lenders, we understand the importance of transparency, accuracy, and accountability in the services we offer and take pride in the quality of work we provide.</h3>
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h3>Celebal Technologies recognized as the winner of 2022 Microsoft India Partner of the Year for the 2nd time in a row</h3>
+}
